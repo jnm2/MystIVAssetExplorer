5312a6a99eda3aa9b5066f703fe5ee56eff7cc47
jnm2 <me@jnm2.com>
2025-08-29 21:32:05 -0400

    Enable browsing and exporting sound files

--- MystIVAssetExplorer/ViewModels/AssetFolderListingFile.cs
+++ MystIVAssetExplorer/ViewModels/AssetFolderListingFile.cs
@@ -1,11 +1,20 @@
 ﻿using MystIVAssetExplorer.Formats;
+using System.IO;
+using System.Threading.Tasks;
 
 namespace MystIVAssetExplorer.ViewModels;
 
-public class AssetFolderListingFile(M4bFile file) : AssetFolderListing
+public class AssetFolderListingFile(M4bFile file) : AssetFolderListing, IExportableFolderListing
 {
     public M4bFile File { get; } = file;
 
     public override string Name => File.Name;
     public override int? Size => File.Size;
+
+    public async Task ExportToStreamAsync(Stream stream)
+    {
+        await stream.WriteAsync(File.Memory);
+    }
+
+    public string GetExportFileName() => File.Name;
 }
