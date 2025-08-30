4c06b3edffafeac4a876306137dde60cfcbf4fc2
jnm2 <me@jnm2.com>
2025-08-29 22:56:04 -0400

    Add ability to play audio in-app

---
+++ MystIVAssetExplorer/BoolToObjectConverter.cs
@@ -0,0 +1,27 @@
+﻿using Avalonia.Data.Converters;
+using Avalonia.Markup.Xaml;
+using System;
+using System.Globalization;
+
+namespace MystIVAssetExplorer;
+
+public sealed class BoolToObjectConverter : MarkupExtension, IValueConverter
+{
+    public object? TrueValue { get; set; }
+    public object? FalseValue { get; set; }
+
+    public object? Convert(object? value, Type targetType, object? parameter, CultureInfo culture)
+    {
+        return (bool)value! ? TrueValue : FalseValue;
+    }
+
+    public object? ConvertBack(object? value, Type targetType, object? parameter, CultureInfo culture)
+    {
+        return Equals(TrueValue, value);
+    }
+
+    public override object ProvideValue(IServiceProvider serviceProvider)
+    {
+        return this;
+    }
+}
